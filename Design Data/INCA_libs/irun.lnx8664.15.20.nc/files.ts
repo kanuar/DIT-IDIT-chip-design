c0814e465b1b36d6126347a460e916030c2bc486
raunak kodwani <68312266+kanuar@users.noreply.github.com>
2023-11-10 19:22:24 +0530

    Add files via upload

---
+++ Design Data/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,12 @@
+1698767617 /home/cmos/cds.lib
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/add.v
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/mul.v
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/sub.v
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/bfly2_4.v
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/bfly4_4.v
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/dit_fft_point8.v
+1699295631 /home/cmos/Desktop/DSD_FINAL_PROJECT/code_cov_testbench.v
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/bbfly2_4.v
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/bbfly4_4.v
+1699258384 /home/cmos/Desktop/DSD_FINAL_PROJECT/idif_fft_8.v
+1699296238 /home/cmos/Desktop/DSD_FINAL_PROJECT/code_cov_reverse_testbench.v
---
+++ Design Data/fv/dit_fft_8/read_libs.tcl
@@ -0,0 +1,3 @@
+add_search_path . /home/install/GENUS201/tools.lnx86/lib/tech -library -both
+read_library -liberty -both /home/cmos/Desktop/DSD_FINAL_PROJECT/saed90nm_typ.lib /home/cmos/Desktop/DSD_FINAL_PROJECT/saed90nm_typ.lib
+
